d72cf44b0e54faefaef4f842843bda743b2fb1c6
Dallin Sly <krave83@gmail.com>
2023-04-30 22:03:45 -0400

    final submit 5

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -2,62 +2,62 @@ import "./WeatherCard.css";
 
 const weatherOptions = [
   {
-    url: require("../images/weather/day-sunny.svg").default,
+    url: require("../../images/weather/day-sunny.svg").default,
     day: true,
     type: "sunny",
   },
   {
-    url: require("../images/weather/day-storm.svg").default,
+    url: require("../../images/weather/day-storm.svg").default,
     day: true,
     type: "storm",
   },
   {
-    url: require("../images/weather/day-snow.svg").default,
+    url: require("../../images/weather/day-snow.svg").default,
     day: true,
     type: "snow",
   },
   {
-    url: require("../images/weather/day-rain.svg").default,
+    url: require("../../images/weather/day-rain.svg").default,
     day: true,
     type: "rain",
   },
   {
-    url: require("../images/weather/day-fog.svg").default,
+    url: require("../../images/weather/day-fog.svg").default,
     day: true,
     type: "fog",
   },
   {
-    url: require("../images/weather/day-cloudy.svg").default,
+    url: require("../../images/weather/day-cloudy.svg").default,
     day: true,
     type: "cloud",
   },
   {
-    url: require("../images/weather/night-sunny.svg").default,
+    url: require("../../images/weather/night-sunny.svg").default,
     day: false,
     type: "moon",
   },
   {
-    url: require("../images/weather/night-storm.svg").default,
+    url: require("../../images/weather/night-storm.svg").default,
     day: false,
     type: "stormy",
   },
   {
-    url: require("../images/weather/night-snow.svg").default,
+    url: require("../../images/weather/night-snow.svg").default,
     day: false,
     type: "snowy",
   },
   {
-    url: require("../images/weather/night-rain.svg").default,
+    url: require("../../images/weather/night-rain.svg").default,
     day: false,
     type: "rainy",
   },
   {
-    url: require("../images/weather/night-fog.svg").default,
+    url: require("../../images/weather/night-fog.svg").default,
     day: false,
     type: "foggy",
   },
   {
-    url: require("../images/weather/night-cloudy.svg").default,
+    url: require("../../images/weather/night-cloudy.svg").default,
     day: false,
     type: "cloudy",
   },
