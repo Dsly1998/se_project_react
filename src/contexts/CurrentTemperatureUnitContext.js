9f489456bd324731192f7eedba4aaf214d6b1abc
Dallin Sly <krave83@gmail.com>
2023-08-11 19:08:18 -0400

    fix

--- src/contexts/CurrentTemperatureUnitContext.js
+++ src/contexts/CurrentTemperatureUnitContext.js
@@ -3,6 +3,13 @@ import React from "react";
 const currentTemperatureUnitContext = React.createContext({
   currentTemperatureUnit: "",
   handleToggleSwitchChange: () => {},
+  handleToggleSwitchChange() => {
+    console.log()
+  }
+
+  currentTemperatureUnit() => {
+    console.log()
+  }
 });
 
 export { currentTemperatureUnitContext };
