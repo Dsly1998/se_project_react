00382ed3b572c83555228134a4cd9cad2f835530
Dallin Sly <krave83@gmail.com>
2023-12-14 11:15:19 -0500

    final save 3

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,10 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer__container">
       <div className="footer__left">Developed by Dallin Sly</div>
       <div className="footer__right">2023</div>
+      </div>
     </footer>
   );
 };
